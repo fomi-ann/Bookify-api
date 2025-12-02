be1276decd5103bdbfaf15f29c53a27848cb1f71
katikoppel <katikoppel1995@gmail.com>
2025-12-02 12:41:51 +0200

    As an app developer I want to get all books in my app. #435859

--- controllers/BooksController.js
+++ controllers/BooksController.js
@@ -2,3 +2,10 @@ const {db} = require('../db')
 const Utilities = require('./Utilities')
 const UUID = require('uuid')
 
+exports.getAll = async(req, res) => {
+    const books = await db.books.findAll();
+    console.log("getAll: "+ books);
+    res
+    .status(200)
+    .send(books.map(({BookID, Name}) => {return{BookID, Name}}))
+}
