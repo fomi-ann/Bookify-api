45e75b0f523812e15c34a378a7fd86043588594c
katikoppel <katikoppel1995@gmail.com>
2025-12-14 23:12:47 +0200

    Add correct User routes

--- backend-api/routes/uRoutes.js
+++ backend-api/routes/uRoutes.js
@@ -2,5 +2,10 @@ const UsersController = require("../controllers/UsersController")
 
 module.exports = (app) => {
     app.route("/users")
+    .get(UsersController.getAll)
     .post(UsersController.create)
+    app.route("/users/:UserID")
+    .get(UsersController.getByID)
+    .delete(UsersController.deleteById)
+    .put(UsersController.modifyById)
 }
