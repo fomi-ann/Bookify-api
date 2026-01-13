babb55a4b7d1f3cee083fcbb1af5c475d6e01a99
Anna Fomina <ann.fomi77@gmail.com>
2026-01-13 17:16:49 +0300

    Add style for register form

--- frontend-site/bookify-frontend/src/components/SignUpForm.vue
+++ frontend-site/bookify-frontend/src/components/SignUpForm.vue
@@ -29,37 +29,99 @@
     }
 </script>
 
-<template class="bg-dark">
-    <h1>Enter your data:</h1>
-    <form @submit.prevent="signUp">
-        <div>
-            <label>Email</label>
-            <input v-model="NewUser.Email" type="text"/>
-        </div>        
-        <div>
-            <label>PlainPassword</label>
-            <input v-model="NewUser.PlainPassword" type="text"/>
+<template>
+  <div class="d-flex align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-6 col-lg-5">
+
+            <h1 class="h4 mb-4 text-center">Register Bookify User</h1>
+          <div class="card shadow-sm">
+            <div class="card-body">
+
+              <form @submit.prevent="signUp">
+
+                <div class="mb-3">
+                  <label class="form-label">Email</label>
+                  <input
+                    v-model="NewUser.Email"
+                    type="email"
+                    class="form-control"
+                    placeholder="example@email.com"
+                  />
+                </div>
+
+                <div class="mb-3">
+                  <label class="form-label">Password</label>
+                  <input
+                    v-model="NewUser.PlainPassword"
+                    type="password"
+                    class="form-control"
+                    placeholder="*********"
+                  />
+                </div>
+
+                <div class="mb-3">
+                  <label class="form-label">Username</label>
+                  <input
+                    v-model="NewUser.UserName"
+                    type="text"
+                    class="form-control"
+                    placeholder="user123"
+                  />
+                </div>
+
+                <div class="mb-3">
+                  <label class="form-label">Full name</label>
+                  <input
+                    v-model="NewUser.FullName"
+                    type="text"
+                    class="form-control"
+                    placeholder="John Doe"
+                  />
+                </div>
+
+                <div class="mb-3">
+                  <label class="form-label">Display name</label>
+                  <input
+                    v-model="NewUser.DisplayName"
+                    type="text"
+                    class="form-control"
+                    placeholder="BookWorm123"
+                  />
+                </div>
+
+                <div class="mb-3">
+                  <label class="form-label">Phone number</label>
+                  <input
+                    v-model="NewUser.PhoneNumber"
+                    type="text"
+                    class="form-control"
+                    placeholder="375 54738204"
+                  />
+                </div>
+
+                <div class="mb-4">
+                  <label class="form-label">Profile image URL</label>
+                  <input
+                    v-model="NewUser.ProfileImageUrl"
+                    type="text"
+                    class="form-control"
+                    placeholder="https://url.com/image123"
+                  />
+                </div>
+
+                <button type="submit" class="btn btn-primary w-100">
+                  Register
+                </button>
+
+              </form>
+            </div>
+          </div>
+
         </div>
-        <div>
-            <label>UserName</label>
-            <input v-model="NewUser.UserName" type="text"/>
-        </div>
-        <div>
-            <label>FullName</label>
-            <input v-model="NewUser.FullName" type="text"/>
-        </div>
-        <div>
-            <label>DisplayName</label>
-            <input v-model="NewUser.DisplayName" type="text"/>
-        </div>
-        <div>
-            <label>PhoneNumber</label>
-            <input v-model="NewUser.PhoneNumber" type="text"/>
-        </div>
-        <div>
-            <label>ProfileImageUrl</label>
-            <input v-model="NewUser.ProfileImageUrl" type="text"/>
-        </div>
-        <button type="submit">Register</button>
-    </form>
+      </div>
+    </div>
+  </div>
 </template>
+
--- frontend-site/bookify-frontend/src/main.js
+++ frontend-site/bookify-frontend/src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap JS (optional but recommended)
+import 'bootstrap'
+
 createApp(App).use(router).mount('#app')
