4899a575723a435efd9ad3ff9290ba541d7e5ef4
Anna Fomina <ann.fomi77@gmail.com>
2026-01-20 13:41:32 +0300

    add user logout button to frontend

--- frontend-site/bookify-frontend/src/App.vue
+++ frontend-site/bookify-frontend/src/App.vue
@@ -1,23 +1,72 @@
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      // local storage
+      user: JSON.parse(localStorage.getItem('bookify_user'))
+    }
+  },
+  watch: {
+    '$route'() {
+      this.user = JSON.parse(localStorage.getItem('bookify_user'));
+    }
+  },
+  methods: {
+    async handleLogout() {
+      try {
+        const response = await fetch(`http://localhost:8080/auth/logout`, {
+          method: 'DELETE',
+          credentials: 'include'
+        });
+
+        if (response.ok) {
+          localStorage.removeItem('bookify_user');
+          this.user = null;
+          
+
+          this.$router.push('/login');
+        }
+      } catch (error) {
+        console.error("Logout error:", error);
+
+        localStorage.removeItem('bookify_user');
+        this.user = null;
+        this.$router.push('/login');
+      }
+    }
+  }
+}
+</script>
 <template>
-  <nav class="navbar bg-body-tertiary px-5">
-  <div class="container">
+  <nav class="navbar bg-body-tertiary px-lg-5 px-3">
+    <div class="container-fluid">
+      <router-link class="navbar-brand d-flex align-items-center" to="/">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bookmark-fill text-primary" viewBox="0 0 16 16">
+          <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
+        </svg>
+        <span class="ms-2 fw-bold">Bookify</span>
+      </router-link>
 
-      <router-link  class="navbar-brand" to="/">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
-        <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
-      </svg>
-    </router-link>
+      <div class="nav-links p-2">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/books">Books</router-link>
 
-    </div>
-    <div>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/books">Books</router-link> |
-    <router-link to="/signup">Register</router-link> |
-    <router-link to="/login">Log In</router-link>
+        <span v-if="!user">
+          | <router-link to="/signup">Register</router-link> |
+          <router-link to="/login">Log In</router-link>
+        </span>
+
+        <span v-else>
+          | <a href="#" @click.prevent="handleLogout" class="text-danger fw-bold text-decoration-none">Logout</a>
+          | <span class="text-muted small ms-1">Hi, <b>{{ user.DisplayName }}</b></span>
+        </span>
+      </div>
     </div>
   </nav>
-  <router-view/>
+
+  <router-view />
 </template>
 
 <style>
